42b7708766935a4c84fcd6d835ed0a6373a3244d
nattanonsuwannapha <nattanonsuwannapha@gmail.com>
2026-09-10 15:35:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("hello javascript");
+console.log("hello Computer Science");
+document.getElementById("text").innerHTML = "Nattanon Suwannapha";
+ document.write("Hello document write");
+window.alert("welcome to my page");
