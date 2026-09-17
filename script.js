5b4598d4e3867c5280ff3c90653eeb7e75ecb0d8
nattanonsuwannapha <nattanonsuwannapha@gmail.com>
2026-09-17 15:40:14 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-console.log("hello javascript");
-console.log("hello Computer Science");
-document.getElementById("text").innerHTML = "Nattanon Suwannapha";
- document.write("Hello document write");
-window.alert("welcome to my page");
